cdd9663881ae7cb45a0142f191d21580d768dd45
John Rooney <rooneyjohn@gmail.com>
2022-11-16 17:12:42 -0500

    commit tree 1ebd68cda68e6f0beba3a00f2658831760800027

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const myArray = [];
+function writeCards(myNames, events){
+    for (let index = 0; index < myNames.length; index++) {
+        myArray[index] = `Thank you, ${myNames[index]}, for the wonderful ${events} gift!`
+    }
+    return myArray 
+}
+
+function countDown(myNumber){
+    if (myNumber >= 0) {
+        while (myNumber >= 0) {
+            console.log(myNumber)
+            myNumber--;
+        }
+    }else{
+        console.log("That number isn't above Zero.")
+    }
+}
